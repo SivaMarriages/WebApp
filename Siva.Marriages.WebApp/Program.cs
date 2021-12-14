9559f58ad6f3a9fa9a9717487111a4ec0cadfaea
kiran <vlskirankumar@gmail.com>
2021-12-14 19:13:08 +0530

    first cut

---
+++ Siva.Marriages.WebApp/Program.cs
@@ -0,0 +1,41 @@
+using Microsoft.AspNetCore.SpaServices.AngularCli;
+using System.Diagnostics;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseRouting();
+
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller}/{action=Index}/{id?}");
+
+if (Debugger.IsAttached)
+{
+    app.UseSpa(builder =>
+    {
+        builder.Options.SourcePath = "ClientApp";
+        builder.UseAngularCliServer(npmScript: "start");
+    });
+}
+else
+{
+    app.UseSpaStaticFiles();
+}
+
+app.Run();
