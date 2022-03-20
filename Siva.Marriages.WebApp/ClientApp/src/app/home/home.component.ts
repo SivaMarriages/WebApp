90f8a978e7f3eb2fb2fad91fe8add5338bd22026
lvalluri <lskirankumar.v@factset.com>
2022-03-20 17:05:30 +0530

    fix few UI align in home page

--- Siva.Marriages.WebApp/ClientApp/src/app/home/home.component.ts
+++ Siva.Marriages.WebApp/ClientApp/src/app/home/home.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
-import { BirthDetails, CandidateProfile, ProfileData } from '../models/profile';
+import { BirthDetails, CandidateProfile, Profession, ProfileData } from '../models/profile';
 import { ProfileService } from '../shared/profile.service';
 import { UIService } from '../shared/UIService';
 
@@ -9,6 +9,7 @@ import { UIService } from '../shared/UIService';
   templateUrl: './home.component.html',
 })
 export class HomeComponent {
+  filterText = "";
   profiles: CandidateProfile[];
   filterdProfiles: CandidateProfile[];
   profilesDataStr: string[];
@@ -19,14 +20,14 @@ export class HomeComponent {
     retStr += data.motherMaidenname;
     retStr += data.nativePlace;
     retStr += data.gender.toString();
+    retStr += data.contactDetails.number1;
+    retStr += data.contactDetails.number2;
+    retStr += data.contactDetails.email;
     retStr += data.birthDetails.dateOfBirth;
     retStr += data.birthDetails.timeOfBirth;
     retStr += data.birthDetails.placeOfBirth;
     retStr += data.birthDetails.rasi;
     retStr += data.birthDetails.nakshatra;
-    retStr += data.contactDetails.number1;
-    retStr += data.contactDetails.number2;
-    retStr += data.contactDetails.email;
     retStr += data.profession.designation;
     retStr += data.profession.companyName;
     retStr += data.profession.place;
@@ -58,17 +59,48 @@ export class HomeComponent {
     return (data.birthDetails && data.birthDetails.dateOfBirth ? `Age: ${(new Date()).getFullYear() - (new Date(data.birthDetails.dateOfBirth)).getFullYear()} years` : '');
   }
   getCardBirth(data: BirthDetails): string {
-    return `${data.dateOfBirth} ${data.timeOfBirth} ${data.placeOfBirth} ${data.rasi} ${data.nakshatra}`;
+    let ret = "";
+    if (data.dateOfBirth !== "") {
+      ret += (new Date(data.dateOfBirth)).toDateString() + ", ";
+    }
+    if (data.timeOfBirth !== "") {
+      ret += data.timeOfBirth + ", ";
+    }
+    if (data.rasi !== undefined && data.rasi.toString() !== "") {
+      ret += data.rasi + ", ";
+    }
+    if (data.nakshatra !== undefined && data.nakshatra.toString() !== "") {
+      ret += data.nakshatra + ", ";
+    }
+
+    if (ret.length > 0) {
+      ret = ret.slice(0, ret.length - 2)
+    }
+    return ret;
   }
-  getParents(data: ProfileData): string {
-    return `Father: ${data.father}, Mother:${data.mother}`;
+  getWorkDetails(profession: Profession): string {
+    let ret = "";
+    if (profession.designation !== "") {
+      ret += profession.designation + ", ";
+    }
+    if (profession.companyName !== "") {
+      ret += profession.companyName + ", ";
+    }
+    if (profession.place !== "") {
+      ret += profession.place + ", ";
+    }
+
+    if (ret.length > 0) {
+      ret = ret.slice(0, ret.length - 2)
+    }
+    return ret;
   }
   getProfilePictureUrl(data: string[]) {
     return `api/profilePictures/${data[0]}`;
   }
 
-  applyFilter(event: any) {
-    const filter = event.target.value.toLowerCase();
+  applyFilter() {
+    const filter = this.filterText.toLowerCase();
     this.filterdProfiles = [];
     for (let i = 0; i < this.profiles.length; i++) {
       if (this.profilesDataStr[i].includes(filter)) {
